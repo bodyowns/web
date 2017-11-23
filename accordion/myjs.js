ebbeb653e98930a5c39777c4650ac0b93fe5cfc3
bodyowns <body2pgl@gmail.com>
2017-11-23 08:05:58 +0000

    Fetching text from api

--- accordion/myjs.js
+++ accordion/myjs.js
@@ -1,3 +1,5 @@
+// accordion
+
 var aco = document.getElementsByClassName("accordion");
 var i;
 
@@ -12,3 +14,51 @@ for (i = 0; i < aco.length; i++) {
         }
     };
 }
+
+
+// Create Event Listener
+
+document.getElementById('btn').addEventListener
+('click', loadText);
+
+function loadText(){
+    // Create XHR Object
+    var xhr = new XMLHttpRequest();
+
+    // open -> type, url, async
+    xhr.open('GET', 'http://design.propcom.co.uk/buildtest/accordion-data.json', true)
+
+    console.log('Ready State: ', xhr.readyState);
+    //Http Status Codes
+
+    //200: 'Ok'
+    //403: 'Forbidden'
+    //404: 'Not Found'
+
+    // Ready State Vals
+
+    // 0: request not initilized
+    // 1: server connection established
+    // 2: request received
+    // 3: processing request
+    // 4: request finished and response is ready
+
+
+    xhr.onreadystatechange = function(){
+
+        console.log('Ready State: ', xhr.readyState);
+            var headers = JSON.parse(this.responseText);
+        document.getElementById('btn').innerHTML = headers.blocks[0].heading;
+        document.getElementById('btn2').innerHTML = headers.blocks[1].heading;
+        document.getElementById('btn3').innerHTML = headers.blocks[2].heading;
+        document.getElementById('cnt').innerHTML = headers.blocks[0].content;
+        document.getElementById('cnt2').innerHTML = headers.blocks[1].content;
+        document.getElementById('cnt3').innerHTML = headers.blocks[2].content;
+
+
+
+    };
+};
+    //send request
+//xhr.send();
+
